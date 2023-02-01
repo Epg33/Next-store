302bd2a7b2ec21d395ef761433ad7b789d0c4736
Ethiem Guerrero <ethiemalex03@gmail.com>
2023-02-01 14:21:37 -0500

    planear bien el home y el manejo de las imagenes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,40 @@
-export default function Home() {
+import Image from "next/image"
+
+interface product {
+  id: number,
+  title: string,
+  price: number,
+  description: string,
+  category: string,
+  image: string,
+  rating: { rate: number, count: number }
+}
+
+const fetchProducts = async ():Promise<product[]> => {
+  const data = await fetch("https://fakestoreapi.com/products")
+  const res = await data.json();
+  return res
+}
+
+export default async function Home() {
+  const products = await fetchProducts();
   return (
-    <main className="w-full h-screen grid place-content-center bg-slate-900">
+    <main className="w-full h-fit flex flex-col items-start bg-slate-900">
       <h1 className="text-6xl text-slate-100">NEXT STORE</h1>
+      <div className="w-full grid grid-cols-3 gap-2">
+        {
+          products.map(product=> {
+            return <div key={product.id} className="w-3/4 h-fit bg-slate-800">
+              <h4>{product.title}</h4>
+              <p>{product.price}</p>
+              <p>{product.description}</p>
+              <span>{product.category}</span>
+              <Image src={product.image} alt={`image for the ${product.title}`} width={680} height={770} className="w-full h-auto"/>
+              <p>{product.rating.rate}</p>
+            </div>
+          })
+        }
+      </div>
     </main>
-  )
+  );
 }
