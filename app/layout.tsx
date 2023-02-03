0ae9d7b4bffe07442233bb01a67633e74d9d81bc
Ethiem Guerrero <ethiemalex03@gmail.com>
2023-02-03 14:59:42 -0500

    nav bar hecho, agregar animacion

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,18 @@
 
 import { Nav } from '@/components';
 import { useState } from 'react';
+import { HiMenu } from 'react-icons/hi'
 import './globals.css';
 
 export default function RootLayout({ children }: {children: React.ReactNode }) {
-  const [showNav, setShowNav] = useState<boolean>(true);
+  const [showNav, setShowNav] = useState<boolean>(false);
   return (
     <html lang="en">
       <body className="w-full h-screen flex flex-col items-start bg-slate-900">
+        <header className="w-full flex justify-between items-center text-slate-100 pt-4 px-4">
+          <h1 className="text-2xl">Next Store</h1>
+          <button onClick={()=>setShowNav(true)}><HiMenu /></button>
+        </header>
         <Nav inView={showNav} setShowNav={setShowNav}/>
         {children}
       </body>
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Dispatch, SetStateAction } from "react";
+import {FaHome, FaShoppingBag, FaFolder, FaShoppingCart, FaUserCircle, FaUserFriends} from 'react-icons/fa'
 
 interface NavBar {
   inView: boolean;
@@ -10,16 +11,17 @@ const Nav: React.FC<NavBar> = ({ inView, setShowNav }): JSX.Element | null => {
   if (!inView) return null;
   return (
     <>
-      <div className="w-full h-screen fixed flex justify-center items-center bg-transparent opacity-75 blur-xl"></div>
-      <div className="w-full h-screen fixed flex flex-col justify-center items-center bg-transparent p-96">
-        <button className="text-2xl text-slate-100 place-self-end" onClick={()=>setShowNav(false)}>X</button>
-        <nav>
-          <Link href="/">Home</Link>
-          <Link href="/products">Products</Link>
-          <Link href="/categorys">Categorys</Link>
-          <Link href="/carts">Carts</Link>
-          <Link href="/users">Users</Link>
-          <Link href={`/users/${1}`}>Profile</Link>
+      <div className="w-full h-screen fixed flex z-20 flex-col justify-start items-end backdrop-blur-sm inset-0 pr-6 pt-4">
+        <nav className="bg-slate-700 h-3/5 w-5/6 flex flex-col items-start gap-5 pt-4 pl-4 rounded-xl sm:w-3/5 md:w-2/4 lg:w-1/4">
+          <div className="h-[10%] w-full flex items-center">
+            <Link className="h-[10%] w-11/12 flex items-center gap-2 text-slate-100 text-xl py-4 pl-2" href="/"><FaHome /> Home</Link>
+            <button className="text-2xl text-slate-100 place-self-end mb-4" onClick={()=>setShowNav(false)}>X</button>
+          </div>
+          <Link className="h-[10%] w-11/12 flex items-center gap-2 text-slate-100 text-xl py-4 pl-2" href="/products"><FaShoppingBag /> Products</Link>
+          <Link className="h-[10%] w-11/12 flex items-center gap-2 text-slate-100 text-xl py-4 pl-2" href="/categorys"><FaFolder />Categorys</Link>
+          <Link className="h-[10%] w-11/12 flex items-center gap-2 text-slate-100 text-xl py-4 pl-2" href="/carts"><FaShoppingCart />Carts</Link>
+          <Link className="h-[10%] w-11/12 flex items-center gap-2 text-slate-100 text-xl py-4 pl-2" href="/users"><FaUserFriends />Users</Link>
+          <Link className="h-[10%] w-11/12 flex items-center gap-2 text-slate-100 text-xl py-4 pl-2" href={`/users/${1}`}><FaUserCircle />Profile</Link>
         </nav>
       </div>
     </>
