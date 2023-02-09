14a39248898c083d0f658d692fad0684ddcbb2f1
Ethiem Guerrero <ethiemalex03@gmail.com>
2023-02-09 13:56:09 -0500

    un poco del home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+import Image from "next/dist/client/image";
+import { fetchByCategory, fetchingHome } from "../services/fetching";
+
 export default async function Home() {
+  const womansClothes = await fetchByCategory("women's clothing");
+  console.log(womansClothes);
   return (
-    <main>
-      
+    <main className="text-slate-100 pt-16">
+      <section className="w-full flex flex-col gap-5 items-center px-4">
+        <h4 className="text-xl">Latest in women's clothes</h4>
+        {
+          womansClothes.map((piece, i)=>{
+            return <div key={i} className='w-11/12 h-5/6 flex flex-col gap-1 items-start p-3 bg-slate-800 rounded-xl'>
+              <Image 
+                src={piece.image} alt={piece.description} width={300} height={450} 
+                className="w-full h-auto"/>
+              <h3 className="text-sm">{piece.title}</h3>
+              <p className="text-cyan-500">{piece.price} $</p>
+            </div>
+          })
+        }
+      </section>
     </main>
   );
 }
--- services/fetching.ts
+++ services/fetching.ts
@@ -1,5 +1,24 @@
-export const fetchingHome = async () => {
-  const res = await fetch();
-  const data = res.json();
+export interface products {
+  id: number,
+  title: string,
+  price: number,
+  description: string,
+  category: string,
+  image: string,
+  rating : {
+    rate: number,
+    count: number
+  }
+}
+
+export const fetchingHome = async ():Promise<products[]> => {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const data: Promise<products[]> = res.json();
+  return data
+}
+
+export const fetchByCategory = async (category: string ):Promise<products[]> => {
+  const res = await fetch(`https://fakestoreapi.com/products/category/${category}`);
+  const data: Promise<products[]> = res.json();
   return data
 }
