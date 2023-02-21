e572a73c1225834a7c48187de02a5a24b19fe51c
Ethiem Guerrero <ethiemalex03@gmail.com>
2023-02-21 14:54:21 -0500

    not found page

--- app/products/not-found.tsx
+++ app/products/not-found.tsx
@@ -2,7 +2,12 @@
 
 const NotFound = () => {
   return (
-    <h1 className="mt-24 text-slate-100 text-9xl">not-found</h1>
+    <section className='h-screen w-full grid place-content-center'>
+      <div className='flex flex-col items-center gap-12 text-slate-100'>
+        <h1 className='relative w-full text-center text-9xl tracking-widest box-reflect'>404</h1>
+        <p>Page not found...</p>
+      </div>
+    </section>
   )
 }
 
